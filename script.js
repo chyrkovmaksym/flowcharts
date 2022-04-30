c8f0e8464bd1c3d72c415033ae608d6b19a8b73a
Maksym <chyrkovmaksym1@gmail.com>
2022-04-30 23:59:46 +0300

    Added ability to detect nested blocks

--- script.js
+++ script.js
@@ -7,47 +7,146 @@ button.addEventListener('click', () => {
 
 class BlockBuilder {
   constructor(text) {
+    this.id = 0;
+    this.prevId = 0;
     this.res = [];
+    text = text.replaceAll('}', '}\n');
     this.strArray = text.split(/\r\n|\r|\n/g);
-    this.findKeyWords();
+    this.strArray = this.cleanCode(this.strArray);
+    this.findKeyWords(this.strArray, 0);
     console.log(this.res);
   }
-  resGeneration(type, text) {
+  cleanCode(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = arr[i].trimStart();
+      arr[i] = arr[i].trimEnd();
+    }
+    arr = arr.filter((str) => str !== '');
+    return arr;
+  }
+  insideRoundBrackets(txt) {
+    let counter = 0;
+    let startId;
+    let endId;
+    const splitedString = txt.split('');
+    for (let i = 0; i < splitedString.length; i++) {
+      if (splitedString[i] === '(') {
+        if (counter === 0) {
+          startId = i;
+        }
+        counter++;
+      }
+      if (splitedString[i] === ')') {
+        counter--;
+        if (counter === 0) {
+          endId = i;
+          break;
+        }
+      }
+    }
+    return [startId, endId];
+  }
+  insideCurlyBrackets(txt) {
+    let counter = 0;
+    let startId;
+    let endId;
+    for (let i = 0; i < txt.length; i++) {
+      if (txt[i].includes('{')) {
+        if (counter === 0) {
+          startId = i;
+        }
+        counter++;
+      } else if (txt[i].includes('}')) {
+        counter--;
+        if (counter === 0) {
+          endId = i;
+          break;
+        }
+      }
+    }
+    return [startId, endId];
+  }
+  resGeneration(type, text, id, prevId) {
     this.res.push({
       type: type,
-      text:
-        text.match(/\(([^)]+)\)/) !== null
-          ? text.match(/\(([^)]+)\)/)[1]
-          : 'no round brackets',
+      text: text,
+      id: id,
+      prevId: prevId,
     });
   }
-  findKeyWords() {
-    for (let str of this.strArray) {
-      str = str.trimStart();
+  static currArr = [];
+
+  nestedBlocks(keyWord, arr, str, i, previousId) {
+    let currRowLimits = [];
+    let currStrLimits = [];
+    this.id++;
+    currStrLimits = this.insideRoundBrackets(str);
+
+    this.resGeneration(
+      keyWord,
+      str
+        .split('')
+        .slice(currStrLimits[0], currStrLimits[1] + 1)
+        .join(''),
+      this.id,
+      previousId
+    );
+    if (
+      str.split('')[str.split('').length - 1] === '{' ||
+      arr[i + 1].trimStart().split('')[0] === '{'
+    ) {
+      BlockBuilder.currArr = arr.slice(i, arr.length);
+      currRowLimits = this.insideCurlyBrackets(BlockBuilder.currArr);
+      this.prevId = this.id;
+      this.findKeyWords(
+        BlockBuilder.currArr.slice(currRowLimits[0] + 1, currRowLimits[1]),
+        this.prevId
+      );
+      return i + currRowLimits[1];
+    } else {
+      if (str.split('')[str.split('').length - 1] === ';') {
+        this.prevId = this.id;
+        this.findKeyWords(
+          [
+            str
+              .split('')
+              .slice(currStrLimits[1] + 1, str.split('').length)
+              .join(''),
+          ],
+          this.prevId
+        );
+      } else {
+        this.prevId = this.id;
+        this.findKeyWords([arr[i + 1]], this.prevId);
+        i++;
+      }
+    }
+    return i;
+  }
+  findKeyWords(arr, previousId) {
+    BlockBuilder.currArr = arr;
+    for (let i = 0; i < arr.length; i++) {
+      let str = arr[i];
       if (
-        str.includes('if') &&
-        !str.includes('print') &&
-        !str.includes('scan')
+        str.trimStart().split('').slice(0, 7).join('').includes('print') ||
+        str.trimStart().split('').slice(0, 7).join('').includes('scan')
       ) {
-        this.resGeneration('if', str);
+        this.id++;
+        this.resGeneration('print/scan', str, this.id, previousId);
       } else if (
-        str.includes('else') &&
-        !str.includes('print') &&
-        !str.includes('scan')
+        str.includes('main') &&
+        str.includes('(') &&
+        str.includes(')')
       ) {
-        this.resGeneration('else', str);
-      } else if (
-        str.includes('while') &&
-        !str.includes('print') &&
-        !str.includes('scan')
-      ) {
-        this.resGeneration('while', str);
-      } else if (
-        str.includes('for') &&
-        !str.includes('print') &&
-        !str.includes('scan')
-      ) {
-        this.resGeneration('for', str);
+        i = this.nestedBlocks('main', arr, str, i, previousId);
+      } else if (str.includes('if')) {
+        i = this.nestedBlocks('if', arr, str, i, previousId);
+      } else if (str.includes('else')) {
+        i = this.nestedBlocks('else', arr, str, i, previousId);
+      } else if (str.includes('while')) {
+        i = this.nestedBlocks('while', arr, str, i, previousId);
+      } else if (str.includes('for')) {
+        i = this.nestedBlocks('for', arr, str, i, previousId);
       }
     }
   }
