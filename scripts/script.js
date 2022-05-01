44a7333299de44528027deaf52e53ca7bde4997a
olegoofy <martynenkooleg77@gmail.com>
2022-05-01 10:23:31 +0300

    Added metods and CSS styles

--- scripts/script.js
+++ scripts/script.js
@@ -16,6 +16,7 @@ class BlockBuilder {
     this.findKeyWords(this.strArray, 0);
     console.log(this.res);
   }
+
   cleanCode(arr) {
     for (let i = 0; i < arr.length; i++) {
       arr[i] = arr[i].trimStart();
@@ -24,6 +25,7 @@ class BlockBuilder {
     arr = arr.filter((str) => str !== '');
     return arr;
   }
+
   insideRoundBrackets(txt) {
     let counter = 0;
     let startId;
@@ -46,6 +48,7 @@ class BlockBuilder {
     }
     return [startId, endId];
   }
+
   insideCurlyBrackets(txt) {
     let counter = 0;
     let startId;
@@ -66,6 +69,7 @@ class BlockBuilder {
     }
     return [startId, endId];
   }
+
   resGeneration(type, text, id, prevId) {
     this.res.push({
       type: type,
@@ -74,6 +78,7 @@ class BlockBuilder {
       prevId: prevId,
     });
   }
+
   static currArr = [];
 
   nestedBlocks(keyWord, arr, str, i, previousId) {
@@ -83,37 +88,37 @@ class BlockBuilder {
     currStrLimits = this.insideRoundBrackets(str);
 
     this.resGeneration(
-      keyWord,
-      str
-        .split('')
-        .slice(currStrLimits[0], currStrLimits[1] + 1)
-        .join(''),
-      this.id,
-      previousId
+        keyWord,
+        str
+            .split('')
+            .slice(currStrLimits[0], currStrLimits[1] + 1)
+            .join(''),
+        this.id,
+        previousId
     );
     if (
-      str.split('')[str.split('').length - 1] === '{' ||
-      arr[i + 1].trimStart().split('')[0] === '{'
+        str.split('')[str.split('').length - 1] === '{' ||
+        arr[i + 1].trimStart().split('')[0] === '{'
     ) {
       BlockBuilder.currArr = arr.slice(i, arr.length);
       currRowLimits = this.insideCurlyBrackets(BlockBuilder.currArr);
       this.prevId = this.id;
       this.findKeyWords(
-        BlockBuilder.currArr.slice(currRowLimits[0] + 1, currRowLimits[1]),
-        this.prevId
+          BlockBuilder.currArr.slice(currRowLimits[0] + 1, currRowLimits[1]),
+          this.prevId
       );
       return i + currRowLimits[1];
     } else {
       if (str.split('')[str.split('').length - 1] === ';') {
         this.prevId = this.id;
         this.findKeyWords(
-          [
-            str
-              .split('')
-              .slice(currStrLimits[1] + 1, str.split('').length)
-              .join(''),
-          ],
-          this.prevId
+            [
+              str
+                  .split('')
+                  .slice(currStrLimits[1] + 1, str.split('').length)
+                  .join(''),
+            ],
+            this.prevId
         );
       } else {
         this.prevId = this.id;
@@ -123,20 +128,64 @@ class BlockBuilder {
     }
     return i;
   }
+
+  findDw(arr, i, previousId, str) {
+    BlockBuilder.currArr = arr.slice(i, arr.length);
+    if (BlockBuilder.currArr[0].includes('while')) {
+      const findInsideWhile = this.insideRoundBrackets(BlockBuilder.currArr[0]);
+      this.resGeneration(
+          'do/while',
+          str
+              .split('')
+              .slice(findInsideWhile[0], findInsideWhile[1] + 1)
+              .join(''),
+          this.id,
+          previousId
+      );
+
+    } else if (BlockBuilder.currArr[1].includes('while')) {
+      const findInsideWhile = this.insideRoundBrackets(BlockBuilder.currArr[1]);
+      this.resGeneration(
+          'do/while',
+          str
+              .split('')
+              .slice(findInsideWhile[0], findInsideWhile[1] + 1)
+              .join(''),
+          this.id,
+          previousId
+      );
+    }
+  }
+  findTern (arr, i, previousId, str){
+    BlockBuilder.currArr = arr.slice(i, arr.length);
+    if (BlockBuilder.currArr[0].includes('?') && BlockBuilder.currArr[0].includes(':')) {
+      const findInsideTernary = this.insideRoundBrackets(BlockBuilder.currArr[0]);
+      this.resGeneration(
+          'ternary operator',
+          str
+              .split('')
+              .slice(findInsideTernary[0], findInsideTernary[1] + 1)
+              .join(''),
+          this.id,
+          previousId
+      );
+    }
+  }
+
   findKeyWords(arr, previousId) {
     BlockBuilder.currArr = arr;
     for (let i = 0; i < arr.length; i++) {
       let str = arr[i];
       if (
-        str.trimStart().split('').slice(0, 7).join('').includes('print') ||
-        str.trimStart().split('').slice(0, 7).join('').includes('scan')
+          str.trimStart().split('').slice(0, 7).join('').includes('print') ||
+          str.trimStart().split('').slice(0, 7).join('').includes('scan')
       ) {
         this.id++;
         this.resGeneration('print/scan', str, this.id, previousId);
       } else if (
-        str.includes('main') &&
-        str.includes('(') &&
-        str.includes(')')
+          str.includes('main') &&
+          str.includes('(') &&
+          str.includes(')')
       ) {
         i = this.nestedBlocks('main', arr, str, i, previousId);
       } else if (str.includes('if')) {
@@ -147,6 +196,15 @@ class BlockBuilder {
         i = this.nestedBlocks('while', arr, str, i, previousId);
       } else if (str.includes('for')) {
         i = this.nestedBlocks('for', arr, str, i, previousId);
+      } else if (str.includes('do')) {
+        i = this.nestedBlocks('do', arr, str, i, previousId);
+        this.findDw(arr, i, previousId, str);
+      } else if (str.includes('?') && str.includes(':')) {
+        this.findTern(arr, i, previousId, str);
+      } else  if (str.includes('switch')) {
+        i = this.nestedBlocks('switch', arr, str, i, previousId);
+      }  else  if (str.includes('case')) {
+        i = this.nestedBlocks('case', arr, str, i, previousId);
       }
     }
   }
