70d86b59273b9021400f2a5136da8d3ba15ccadd
AIDXYLELELE <warboyka48@gmail.com>
2022-05-30 20:42:56 +0300

    fixed

--- scripts/creatinglinks.js
+++ scripts/creatinglinks.js
@@ -1,37 +1,28 @@
-function loadJson(url) {
-    return fetch(url)
-        .then((response) => response.json());
-  }
-  
-  function loadUser(name) {
-    return fetch(`https://api.github.com/users/${name}`)
-        .then((response) => response.json());
-  }
-  
-  function getAvatar(user) {
-    return new Promise(function(resolve, reject) {
-      const img = document.getElementById('img_fot');
-      img.src = user.avatar_url;
-      resolve(user);
-    });
-  }
-  
-  function createLink(name) {
-    return new Promise(function(resolve, reject) {
-      const link = document.getElementById('link_fot');
-      link.href = `https://github.com/${name}`;
-      resolve(name);
-    });
-  }
-  
-  loadJson('https://api.github.com/repos/chyrkovmaksym/course_project/commits')
-      .then((commits) => {
-        return commits[0].author.login;
-      })
-      .then(createLink)
-      .then((user) => loadUser(user))
-      .then(getAvatar)
-      .catch((e) => {
-        throw new Error(e);
-      });
-  
+const BASE_URL = 'https://api.github.com/users/';
+
+const COMMITS_URL = 'https://api.github.com/repos/chyrkovmaksym/course_project/commits';
+
+const loadJson = (url) => fetch(url).then((response) => response.json());
+
+const loadUser = (name) => loadJson(BASE_URL + name);
+
+const getAvatar = (user) => new Promise((resolve) => {
+  const img = document.getElementById('img_fot');
+  img.src = user.avatar_url;
+  resolve(user);
+});
+
+const createLink = (name) => new Promise((resolve) => {
+  const link = document.getElementById('link_fot');
+  link.href = `https://github.com/${name}`;
+  resolve(name);
+});
+
+loadJson(COMMITS_URL)
+  .then((commits) => commits[0].author.login)
+  .then(createLink)
+  .then(loadUser)
+  .then(getAvatar)
+  .catch((e) => {
+    throw new Error(e);
+  });
