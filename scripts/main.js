09cb5f9faad4b7dc6f8742b37de4f13ea27d9caf
AIDXYLELELE <warboyka48@gmail.com>
2022-06-08 17:56:40 +0300

    created reset button

--- scripts/main.js
+++ scripts/main.js
@@ -3,15 +3,20 @@ import { Cleaner } from './cleancode.js';
 import { finder, X, Y } from './test.js';
 
 const code = document.getElementById('code');
-const button = document.getElementById('button');
+const buttonSend = document.getElementById('button-send');
+const buttonClear = document.getElementById('button-clear');
 
 let resultOfProgramm = [];
 let resultOfCleaner = [];
 
-button.addEventListener('click', () => {
+buttonSend.addEventListener('click', () => {
   const cleanedCode = new Cleaner(code.value);
   resultOfCleaner = cleanedCode.getResult();
   const parsedCode = new BlockBuilder(resultOfCleaner);
   resultOfProgramm = parsedCode.getResult();
   finder(resultOfProgramm, X, Y);
+});
+
+buttonClear.addEventListener('click', () => {
+  location.reload();
 });
