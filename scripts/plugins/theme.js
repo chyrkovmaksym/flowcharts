1165593b450cee81c6a0e448eda81fe552dfb618
VLADISLAV_SHEVYROV <warboyka48@gmail.com>
2022-07-15 20:45:09 +0300

    added new plugin

---
+++ scripts/plugins/theme.js
@@ -0,0 +1,18 @@
+const HALF_AN_HOUR = 1800000;
+const HOUR_OF_DARK = 19;
+
+function applyTheme(themeName) {
+  document.querySelector('[title="theme"').setAttribute('href', `styles/${themeName}-theme.css`);
+}
+
+function selectTheme() {
+  const date = new Date();
+  if (date.getHours < HOUR_OF_DARK) {
+    applyTheme('light');
+  } else {
+    applyTheme('dark');
+  }
+  setTimeout(() => selectTheme(), HALF_AN_HOUR);
+}
+
+selectTheme();
