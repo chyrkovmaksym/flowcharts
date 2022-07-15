1ba211331a2e0e50d433adb5831e9678d333cb16
VLADISLAV_SHEVYROV <warboyka48@gmail.com>
2022-07-15 21:19:06 +0300

    fixed some bugs

--- scripts/plugins/theme.js
+++ scripts/plugins/theme.js
@@ -7,11 +7,10 @@ function applyTheme(themeName) {
 
 function selectTheme() {
   const date = new Date();
-  if (date.getHours < HOUR_OF_DARK) {
+  if (date.getHours() < HOUR_OF_DARK) {
     applyTheme('light');
   } else {
     applyTheme('dark');
-    alert(date.getHours);
   }
   setTimeout(() => selectTheme(), HALF_AN_HOUR);
 }
